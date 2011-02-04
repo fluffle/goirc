fb694f154673ccdc7e2b20f31fe642ba42b5f5d7
raylu <raylu@cmu.edu>
2011-02-04 17:26:08 -0500

    Add ignore information to accesslist

--- cmd-access.go
+++ cmd-access.go
@@ -128,6 +128,12 @@ func accesslist(conn *irc.Conn, nick *irc.Nick, args, target string) {
 		say(conn, nick.Nick, "%s is the owner", owner)
 	}
 
+	for i, _ := range(ignores[conn.Network]) {
+		if strings.Contains(i, args) {
+			say(conn, nick.Nick, "%s is being ignored", i)
+		}
+	}
+
 	section := conn.Network + " " + channel
 	users, err := auth.Options(section)
 	if err != nil {
