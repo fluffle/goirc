b32e8d050f76187134acbd8289649f7784cf865e
Alex Bramley <a.bramley@gmail.com>
2011-08-03 08:04:01 +0100

    Gofix for r59.

--- client/handlers.go
+++ client/handlers.go
@@ -218,7 +218,7 @@ func (conn *Conn) h_352(line *Line) {
 		// XXX: do we care about the actual server the nick is on?
 		//      or the hop count to this server?
 		// last arg contains "<hop count> <real name>"
-		a := strings.Split(line.Args[len(line.Args)-1], " ", 2)
+		a := strings.SplitN(line.Args[len(line.Args)-1], " ", 2)
 		n.Name = a[1]
 		if idx := strings.Index(line.Args[6], "*"); idx != -1 {
 			n.Modes.Oper = true
@@ -234,7 +234,7 @@ func (conn *Conn) h_352(line *Line) {
 // Handle 353 names reply
 func (conn *Conn) h_353(line *Line) {
 	if ch := conn.GetChannel(line.Args[2]); ch != nil {
-		nicks := strings.Split(line.Args[len(line.Args)-1], " ", -1)
+		nicks := strings.Split(line.Args[len(line.Args)-1], " ")
 		for _, nick := range nicks {
 			// UnrealIRCd's coders are lazy and leave a trailing space
 			if nick == "" {
